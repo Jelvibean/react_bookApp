d8711d30abe3c48ffddcc3e57837f8192fda9beb
SHR\yelvy.buitrago <yelvyb@yahoo.com>
2017-06-21 17:36:03 -0400

    init project

---
+++ src/containers/book-list.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+
+
+class Booklist extends Component {
+	renderList() {
+		return this.props.books.map((books) => {
+			return (
+				<li 
+					key={books.title}
+					onClick={() => this.props.selectBook(books)} 
+					className="list-group-item">
+					{books.title} 
+				</li>
+			);
+		});
+	}
+
+	render() {
+		return (
+			<ul className="list-group col-sm-4">
+				{this.renderList()}
+			</ul>
+
+		)
+	};
+}
+
+//this is glue between react and redux.
+//
+function mapStateToProps(state){
+	return {
+		books: state.books
+
+	};
+}
+
+
+
+function mapDispatchToProps(dispatch) {
+	//Whenever selectBook is called, the result should be passed to all of our reducers.
+	return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+// Promote BookList from a component to a container - it needs to know about this new dispatch method, selectBook.  Make it available.
+//as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(Booklist)
